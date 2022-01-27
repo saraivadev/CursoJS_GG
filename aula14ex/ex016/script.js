a9ee2afe5961801fd05c067bc4a0ff4ddc44f045
saraivadev <wsaraiva@outlook.com>
2022-01-27 06:31:22 -0300

    Aprimoramente usando select e createElement

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -4,20 +4,35 @@
     var razao = document.getElementById('razao')
     var res = document.getElementById('res')
 
-   
 
-    if (inicio.value == "" || fim.value == ""  || razao.value == "") {
+
+    if (inicio.value == "" || fim.value == "" || razao.value == "") {
         res.innerHTML = `Impossível contar! Preencha todos os campos com numeros.`
-    } else{
-       if (razao.value == 0) {
+    } else {
+
+        i = Number(inicio.value)
+        f = Number(fim.value)
+        r = Number(razao.value)
+
+        if (r == 0) {
             alert("Razão inválida, considerandando razão 1")
-            razao.value = 1
+            r = 1
         }
         res.innerHTML = `<p>Contando: </p>`
-        for (var c = Number(inicio.value); c <= Number(fim.value); c += Number(razao.value)) {
-            res.innerHTML += `${c} <span>&#128073;</span>`
+
+        if (i < f) {
+
+            for (var c = i; c <= f; c += r) {
+                res.innerHTML += `${c} <span>&#128073;</span>`
+            }
+        } else {
+
+            for (var c = i; c >= f; c -= r) {
+                res.innerHTML += `${c} <span>&#128073;</span>`
+            }
         }
         res.innerHTML += `<span>&#127937;</span>`
-    } 
+
+    }
 
 }
--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -3,14 +3,17 @@ function tabuada(){
 
 
 var txtn = document.getElementById('textnum')
-var res = document.getElementById('res')
-if(txtn==""){
+var res = document.getElementById('seltab')
+if(txtn.value.length == 0){
     alert("Digite um número válido")
 } else{
 n = Number(txtn.value)
-
+    res.innerHTML = ""
     for(var c = 1; c<=10; c++){
-    res.innerHTML += `${n} x ${c} = ${n * c}\n`
+    let item = document.createElement('option')
+    item.text = `${n} x ${c} = ${n * c}\n`
+    item.value = `res${c}`
+    res.appendChild(item)
     }
 }
 }
