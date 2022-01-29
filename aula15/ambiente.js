c7ba2c21e4a05367425c841818173ad96f0004d5
saraivadev <wsaraiva@outlook.com>
2022-01-28 22:44:18 -0300

    Aula de Arrays

---
+++ aula15/ambiente.js
@@ -0,0 +1,11 @@
+let num = [5,8,4]
+
+console.log ( num )
+
+num[3] = 6
+
+console.log (num)
+
+num.push(7)
+
+console.log (num)
---
+++ aula15/veornatela.js
@@ -0,0 +1,14 @@
+let valores = [8, 1, 7, 4, 2, 9]
+
+
+console.log(valores)
+/*
+for (let pos =0; pos <= valores.length; pos++){
+    console.log(`A posicao ${pos} tem o valor de ${valores[pos]}`)
+}
+*/
+for (let pos in valores){
+    
+    console.log(`A posicao ${pos} tem o valor de ${valores[pos]}`)
+
+}
