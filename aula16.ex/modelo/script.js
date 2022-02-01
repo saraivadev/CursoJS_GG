8788d02b51b4dfbc8cf0ea27366f52ce61a6a2b9
saraivadev <wsaraiva@outlook.com>
2022-01-31 22:18:54 -0300

    Analisador de numeros funcao addNum() inicio calcular()

--- aula16.ex/modelo/script.js
+++ aula16.ex/modelo/script.js
@@ -6,21 +6,54 @@ function addNum() {
     var res = document.getElementById('res')
 
     let num = Number(txtnum.value)
-    vnum.push(num)
 
-    let numadd = document.createElement('option')
-    numadd.text = `Valor adicionado ${num}`
-    numadd.value = `res${num}`
-    res.appendChild(numadd)
+    if (num == "" || num < 1 || num > 100 || vnum.includes(num)) {
+        alert("Valor inválido ou já encontrado na lista!")
+    } else {
+
+        vnum.push(num)
+
+        let numadd = document.createElement('option')
+        numadd.text = `Valor adicionado ${num}\n`
+        numadd.value = `res${num}`
+        res.appendChild(numadd)
+
+    }
 
 }
 
 
-/*
-
-function calcular(){
-
-}
+/* 
+Função que calculará qtd Nº
+Maior número cadastrado
+Menor número cadastrado
+Soma dos valores
+Media dos valores
 
 */
+
+function calcular() {
+
+    var respostaCalc = document.getElementById('resCalc')
+
+    if (vnum.length == 0) {
+
+        alert("Adicione valores antes de finalizar")
+
+    } else {
+
+     let nmax = Math.max(vnum)
+       respostaCalc.innerHTML = `Ao todo, temos ${vnum.length} números cadastrados <br>`
+       respostaCalc.innerHTML += `O maior valor informado foi ${nmax.value} <br>`
+       respostaCalc.innerHTML += `O menor valor informado foi ${Math.min(vnum)} <br>`
+       respostaCalc.innerHTML += `Somando todos valores, temos <br>`
+       respostaCalc.innerHTML += `A média dos valores digitados é: ${vnum.length} números cadastrados <br>`
+
+x
+    }
+
+
+
+}
+
 
